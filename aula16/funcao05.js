15b5370192eac73655e856ae16e29898c44b60ba
unknown <mael10774@gmail.com>
2021-04-08 14:48:44 -0300

    first commit

---
+++ aula16/funcao05.js
@@ -0,0 +1,20 @@
+// RECURSIVIDADE 
+function fatorial(n){
+    if (n == 1){
+        return 1
+    }else{
+        return n * fatorial(n-1)
+    }
+    
+}
+console.log(fatorial(5))
+
+
+/*
+
+S! = 5 X 4 x 3 x 2 x 1 
+S! = 5 x 4 !
+
+n! = n x  (n-1)!
+1! = 1 
+*/
